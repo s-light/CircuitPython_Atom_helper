097369e125eec51ea5e57494090c630b6f814ab4
s-light <git@s-light.eu>
2021-05-28 22:23:24 +0200

    black formating

--- cp_copy.py
+++ cp_copy.py
@@ -15,6 +15,7 @@ from contextlib import contextmanager
 ##########################################
 # functions
 
+
 @contextmanager
 def cd(newdir):
     """
@@ -33,7 +34,8 @@ def cd(newdir):
 
 ##########################################
 
-class CPCopy(object):
+
+class CPCopy:
     u"""
     Copy CircuitPython scripts or libraries.
 
@@ -57,19 +59,19 @@ class CPCopy(object):
     VERBOSE_DEBUG = 2
 
     def __init__(
-            self,
-            *, #noqa
-            action=ACTION_DEFAULT,
-            path_project=".",
-            filename=None,
-            filename_project=None,
-            path_arduino="",
-            path_uf2="",
-            verbose=0,
-            path_target=None
+        self,
+        *,  # noqa
+        action=ACTION_DEFAULT,
+        path_project=".",
+        filename=None,
+        filename_project=None,
+        path_arduino="",
+        path_uf2="",
+        verbose=0,
+        path_target=None
     ):
         """Init."""
-        super(CPCopy, self).__init__()
+        super()
 
         self.path_script = os.path.dirname(os.path.abspath(__file__))
 
@@ -92,17 +94,13 @@ class CPCopy(object):
             self.path_target = path_target
 
         # force action for arduino files
-        if (
-                (self.filename and self.filename_project)
-                and
-                (self.filename.endswith('ino') or
-                 self.filename_project.endswith('ino'))
+        if (self.filename and self.filename_project) and (
+            self.filename.endswith("ino") or self.filename_project.endswith("ino")
         ):
             self.action = "COPY_ARDUINO_AS_UF2"
             if self.verbose:
                 print(
-                    "arduino file found! changed action to: '{}'"
-                    "".format(self.action)
+                    "arduino file found! changed action to: '{}'" "".format(self.action)
                 )
 
         # create action ~ function mapping
@@ -120,10 +118,7 @@ class CPCopy(object):
         if self.verbose:
             print("action: '{}'".format(self.action))
         if self.verbose and self.verbose >= self.VERBOSE_DEBUG:
-            print(
-                "ACTIONS['{}']".format(self.action),
-                self.ACTIONS[self.action]
-            )
+            print("ACTIONS['{}']".format(self.action), self.ACTIONS[self.action])
         action_function = self.ACTIONS[self.action]
 
         if self.verbose and self.verbose >= self.VERBOSE_DEBUG:
@@ -136,12 +131,11 @@ class CPCopy(object):
         else:
             if self.action != "COPY_ARDUINO_AS_UF2":
                 raise NotADirectoryError(
-                    "no uf2 target disc found. "
-                    "is it mounted correctly? "
+                    "no uf2 target disc found. " "is it mounted correctly? "
                 )
 
         # do action
-        action_function() #noqa
+        action_function()  # noqa
 
         # force sync all things to disk
         if self.verbose:
@@ -153,13 +147,13 @@ class CPCopy(object):
         """Copy as 'main.py'."""
         if self.verbose > 1:
             print(self.copy_as_main.__doc__)
-        self.copy_w_options(destination_filename='main.py')
+        self.copy_w_options(destination_filename="main.py")
 
     def copy_as_code(self):
         """Copy as 'code.py'."""
         if self.verbose > 1:
             print(self.copy_as_code.__doc__)
-        self.copy_w_options(destination_filename='code.py')
+        self.copy_w_options(destination_filename="code.py")
 
     def copy(self):
         """Copy with original filename."""
@@ -195,7 +189,7 @@ class CPCopy(object):
         full_filename_bin = os.path.join("build", filename_bin)
         full_filename_uf2 = os.path.join("build", filename_uf2)
 
-        if sketch_base_dir is "":
+        if sketch_base_dir == "":
             sketch_base_dir = "."
 
         # if self.verbose > 1:
@@ -204,30 +198,29 @@ class CPCopy(object):
 
         with cd(sketch_base_dir):
             if self.verbose:
-                print("*"*42)
+                print("*" * 42)
                 print("compile arduino sketch")
             compile_result = self.compile_arduino_sketch(
-                sketch_filename,
-                path_arduino=self.path_arduino
+                sketch_filename, path_arduino=self.path_arduino
             )
             # print(compile_result)
             if compile_result:
                 raise ValueError("arduino compilation failed!")
             if self.verbose:
-                print("*"*42)
+                print("*" * 42)
                 print("convert to uf2")
             self.convert_to_uf2(
                 full_filename_bin,
                 full_filename_uf2,
                 path_uf2=self.path_uf2,
                 # base_address defaults to 16Byte bootloader (ItsyBitsy M4)
-                base_address="0x4000"
+                base_address="0x4000",
             )
 
         board_found = False
         if not self.path_target:
             if self.verbose:
-                print("*"*42)
+                print("*" * 42)
                 print("activate bootloader")
             # we need to activate the bootloader before we can copy!!
             board_found = self.arduino_reset_board()
@@ -238,8 +231,7 @@ class CPCopy(object):
 
         if board_found:
             if self.path_target:
-                self.copy_uf2_file(
-                    sketch_base_dir, full_filename_uf2, filename_uf2)
+                self.copy_uf2_file(sketch_base_dir, full_filename_uf2, filename_uf2)
             else:
                 raise NotADirectoryError(
                     "no uf2 target disc found. "
@@ -251,11 +243,7 @@ class CPCopy(object):
 
     ##########################################
     def copy_w_options(
-            self,
-            *, #noqa
-            destination_filename=None,
-            compile_to_mpy=False,
-            lib=False
+        self, *, destination_filename=None, compile_to_mpy=False, lib=False  # noqa
     ):
         """Copy with options."""
         source = os.path.join(self.path_project, self.filename_project)
@@ -266,7 +254,8 @@ class CPCopy(object):
         else:
             if lib:
                 destination = os.path.join(
-                    self.path_target, self.path_lib, self.filename_project)
+                    self.path_target, self.path_lib, self.filename_project
+                )
             else:
                 destination = os.path.join(self.path_target, self.filename)
         destination_abs = os.path.abspath(destination)
@@ -330,13 +319,13 @@ class CPCopy(object):
             result = subprocess.check_output(command, universal_newlines=True)
         except subprocess.CalledProcessError as e:
             # print("error handling...")
-            print("*"*42)
+            print("*" * 42)
             print("failed: {}".format(e))
             if self.verbose and self.verbose >= self.VERBOSE_DEBUG:
                 print("detailed output")
                 print(e.output)
             result = e
-            print("*"*42)
+            print("*" * 42)
         else:
             if self.verbose:
                 if self.verbose == 1:
@@ -349,8 +338,7 @@ class CPCopy(object):
             result = None
         return result
 
-    def convert_to_uf2(
-            self, source, destination, path_uf2="", base_address="0x4000"):
+    def convert_to_uf2(self, source, destination, path_uf2="", base_address="0x4000"):
         """Convert to uf2."""
         script = os.path.join(path_uf2, "uf2conv.py")
         script = os.path.expanduser(script)
@@ -403,6 +391,7 @@ class CPCopy(object):
         # open the port at 1200bps and closing again.
         # and than wait untill we have a new port.
         import serial
+
         arduino_port = serial.Serial(baudrate=1200)
         try:
             arduino_port.port = "/dev/ttyACM0"
@@ -426,21 +415,22 @@ class CPCopy(object):
         timeout_duration = 10
         timeout_start = time.monotonic()
         wait_flag = True
-        while (wait_flag):
+        while wait_flag:
             if self.path_target:
                 wait_flag = False
             if self.verbose and self.verbose > self.VERBOSE_DEBUG:
                 print(
-                    "\n"
-                    "time:", time.monotonic() - timeout_start,
-                    "timeout:",  timeout_duration
+                    "\n" "time:",
+                    time.monotonic() - timeout_start,
+                    "timeout:",
+                    timeout_duration,
                 )
             if (time.monotonic() - timeout_start) > timeout_duration:
                 wait_flag = False
             try:
                 time.sleep(1)
                 if self.verbose:
-                    print(".", end='', flush=True)
+                    print(".", end="", flush=True)
             except KeyboardInterrupt as e:
                 print()
                 print("wait_for_new_uf2_disc stoped by KeyboardInterrupt.", e)
@@ -450,15 +440,13 @@ class CPCopy(object):
                 self.path_target = self.get_UF2_disc()
                 if self.verbose and self.verbose > self.VERBOSE_DEBUG:
                     print()
-                    print(
-                        "self.path_target:\n", self.path_target
-                    )
+                    print("self.path_target:\n", self.path_target)
         print()
 
     def copy_uf2_file(self, sketch_base_dir, full_filename_uf2, filename_uf2):
         """Copy uf2 file."""
         if self.verbose:
-            print("*"*42)
+            print("*" * 42)
             print("copy file")
         source = os.path.join(sketch_base_dir, full_filename_uf2)
         source_abs = os.path.abspath(source)
@@ -468,6 +456,7 @@ class CPCopy(object):
             print(source_abs)
             print(destination)
         self.copy_file(source_abs, destination_abs)
+
     ##########################################
 
     def get_UF2_disc(self):
@@ -527,14 +516,16 @@ class CPCopy(object):
                     path_project=self.path_project,
                 )
             )
+
+
 ##########################################
 
 
 def main():
-    """Main."""
-    print(42*'*')
-    print('Python Version: ' + sys.version)
-    print(42*'*')
+    """Handle main."""
+    print(42 * "*")
+    print("Python Version: " + sys.version)
+    print(42 * "*")
 
     print(" ".join(sys.argv))
 
@@ -544,9 +535,7 @@ def main():
     path_arduino_default = ""
     path_uf2_default = ""
 
-    parser = argparse.ArgumentParser(
-        description=CPCopy.__doc__
-    )
+    parser = argparse.ArgumentParser(description=CPCopy.__doc__)
 
     parser.add_argument(
         "-a",
@@ -554,55 +543,45 @@ def main():
         help="what action should i take? (defaults to {})"
         "".format(CPCopy.ACTION_DEFAULT),
         default=CPCopy.ACTION_DEFAULT,
-        choices=CPCopy.ACTIONS
+        choices=CPCopy.ACTIONS,
     )
     parser.add_argument(
         "-p",
         "--path_project",
         help="specify the location for the current project (defaults to {})"
-        "".format(
-            path_project_default
-        ),
-        default=path_project_default
+        "".format(path_project_default),
+        default=path_project_default,
     )
     parser.add_argument(
         "-f",
         "--filename",
         help="specify a location for the input file (defaults to {})"
-        "".format(
-            filename_default
-        ),
-        default=filename_default
+        "".format(filename_default),
+        default=filename_default,
     )
     parser.add_argument(
         "-fp",
         "--filename_project",
         help="specify a location for the input file relative to project."
         "(defaults to {})"
-        "".format(
-            filename_project_default
-        ),
-        default=filename_project_default
+        "".format(filename_project_default),
+        default=filename_project_default,
     )
     parser.add_argument(
         "-pa",
         "--path_arduino",
         help="specify directory with arduino."
         "(defaults to {})"
-        "".format(
-            path_arduino_default
-        ),
-        default=path_arduino_default
+        "".format(path_arduino_default),
+        default=path_arduino_default,
     )
     parser.add_argument(
         "-pu",
         "--path_uf2",
         help="specify directory with uf2 script."
         "(defaults to {})"
-        "".format(
-            path_uf2_default
-        ),
-        default=path_uf2_default
+        "".format(path_uf2_default),
+        default=path_uf2_default,
     )
     # parser.add_argument(
     #     "-c",
@@ -610,16 +589,8 @@ def main():
     #     help="compile file to 'mpy'? (defaults to False)",
     #     action='store_true'
     # )
-    parser.add_argument(
-        '--version',
-        action='version',
-        version='%(prog)s 0.1.0'
-    )
-    parser.add_argument(
-        '-v',
-        '--verbose',
-        action='count'
-    )
+    parser.add_argument("--version", action="version", version="%(prog)s 0.1.0")
+    parser.add_argument("-v", "--verbose", action="count")
     args = parser.parse_args()
 
     cp_copy = CPCopy(
@@ -629,12 +600,12 @@ def main():
         filename_project=args.filename_project,
         path_arduino=args.path_arduino,
         path_uf2=args.path_uf2,
-        verbose=args.verbose
+        verbose=args.verbose,
     )
     cp_copy.process()
 
 
 ##########################################
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
